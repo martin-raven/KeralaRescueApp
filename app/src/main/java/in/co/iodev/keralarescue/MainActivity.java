162fe9dbcdd535215c8ef30270e4e60f66646d8b
martin-raven <martinsiby@gmail.com>
2018-08-16 15:23:45 +0530

    Merged Added battery charge

--- app/src/main/java/in/co/iodev/keralarescue/MainActivity.java
+++ app/src/main/java/in/co/iodev/keralarescue/MainActivity.java
@@ -1,11 +1,23 @@
 package in.co.iodev.keralarescue;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.BatteryManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends Activity {
     Button Malayalam;
@@ -14,14 +26,27 @@ public class MainActivity extends Activity {
     LinearLayout English_layout;
 
 
+    String batteryPercentage;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private View.OnClickListener Inform_btn_listerner = new View.OnClickListener() {
+        public void onClick(View view) {
+            batteryPercentage = getBatteryPercentage();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Malayalam=findViewById(R.id.malayalam);
-        English=findViewById(R.id.english);
-        Malayal_layout=findViewById(R.id.malayalam_layout);
-        English_layout=findViewById(R.id.english_layout);
+
+        Malayalam = findViewById(R.id.malayalam);
+        English = findViewById(R.id.english);
+        Malayal_layout = findViewById(R.id.malayalam_layout);
+        English_layout = findViewById(R.id.english_layout);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         Malayalam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +58,7 @@ public class MainActivity extends Activity {
                 English_layout.setVisibility(View.INVISIBLE);
             }
         });
+
         English.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +71,37 @@ public class MainActivity extends Activity {
             }
         });
 
+        Button Inform_btn = null;
+//        Inform_btn = findViewById()
+//        Inform_btn.setOnClickListener(Inform_btn_listerner);
+        getBatteryPercentage();
+
     }
+
+    String getBatteryPercentage() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        float batteryPct = level / (float) scale;
+        float p = batteryPct * 100;
+        Log.d("Battery percentage", String.valueOf(p));
+        return String.valueOf(Math.round(p));
+    }
+
+    String getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "WIP";
+        }
+        return "WIP";
+    }
+
 
 }
